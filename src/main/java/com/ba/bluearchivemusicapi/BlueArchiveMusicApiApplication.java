dd6a7d1eacb50ed1837f7a045cd67e630b753325
duck <simonchungcck42@gmail.com>
2025-08-17 20:21:11 +0800

    init

---
+++ src/main/java/com/ba/bluearchivemusicapi/BlueArchiveMusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.ba.bluearchivemusicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueArchiveMusicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueArchiveMusicApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ba/bluearchivemusicapi/BlueArchiveMusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ba.bluearchivemusicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueArchiveMusicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

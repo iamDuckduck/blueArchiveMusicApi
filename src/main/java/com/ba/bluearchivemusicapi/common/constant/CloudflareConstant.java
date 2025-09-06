f6bef2cddfc8d4bbd657047a1024303da551569e
duck <simonchungcck42@gmail.com>
2025-09-06 13:54:03 +0800

    constant: create Cloudflare EXPIRATION constants

--- src/main/java/com/ba/bluearchivemusicapi/common/constant/CloudflareConstant.java
+++ src/main/java/com/ba/bluearchivemusicapi/common/constant/CloudflareConstant.java
@@ -1,4 +1,8 @@
 package com.ba.bluearchivemusicapi.common.constant;
 
+import java.time.Duration;
+
 public class CloudflareConstant {
+    public static final Duration OST_AUDIO_EXPIRATION = Duration.ofMinutes(50);
+    public static final Duration OST_IMAGE_EXPIRATION = Duration.ofMinutes(50);
 }

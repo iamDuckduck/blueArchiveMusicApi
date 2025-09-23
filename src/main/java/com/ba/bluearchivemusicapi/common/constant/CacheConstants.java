2c8a5ebab763b7632e585627db90995c1dd8d61b
duck <simonchungcck42@gmail.com>
2025-09-23 19:26:33 +0800

    constant: create PLAYCOUNT_CACHE

--- src/main/java/com/ba/bluearchivemusicapi/common/constant/CacheConstants.java
+++ src/main/java/com/ba/bluearchivemusicapi/common/constant/CacheConstants.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 public class CacheConstants {
     // Cache names
     public static final String AUDIO_URL_CACHE = "audioUrls";
+    public static final String PLAYCOUNT_CACHE = "playCounts";
 
     // TTL durations
     public static final Duration AUDIO_URL_CACHE_TTL = Duration.ofMinutes(40);

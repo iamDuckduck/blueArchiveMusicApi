161eade02805bb203f65caa465be20f1798f6036
duck <simonchungcck42@gmail.com>
2025-09-24 17:04:18 +0800

    feat: create scheduled Task for updating cached playCount into db

--- src/main/java/com/ba/bluearchivemusicapi/repositories/OstRepository.java
+++ src/main/java/com/ba/bluearchivemusicapi/repositories/OstRepository.java
@@ -4,7 +4,9 @@ import com.ba.bluearchivemusicapi.entities.OST;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 
 public interface OstRepository extends CrudRepository<OST, Long>, JpaSpecificationExecutor<OST> {
+    List<OST> findByIdIn(List<Long> ids);
 }
 

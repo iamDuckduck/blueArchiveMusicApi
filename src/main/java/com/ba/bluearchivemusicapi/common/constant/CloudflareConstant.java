7851c54eb79aa6d8dafdfe965df731fc24ee7ba5
duck <simonchungcck42@gmail.com>
2025-10-16 21:35:54 +0800

    feat: create PUBLIC_URL_PREFIX constant

--- src/main/java/com/ba/bluearchivemusicapi/common/constant/CloudflareConstant.java
+++ src/main/java/com/ba/bluearchivemusicapi/common/constant/CloudflareConstant.java
@@ -4,5 +4,5 @@ import java.time.Duration;
 
 public class CloudflareConstant {
     public static final Duration OST_AUDIO_EXPIRATION = Duration.ofMinutes(50);
-    public static final Duration OST_IMAGE_EXPIRATION = Duration.ofMinutes(50);
+    public static final String PUBLIC_URL_PREFIX = "https://pub-185cd71ac9b44750afc23319e9e08130.r2.dev/";
 }

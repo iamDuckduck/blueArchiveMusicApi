8e7e0ab4785605e08274f0acb6ff48be55fe85f4
duck <simonchungcck42@gmail.com>
2026-06-21 14:53:55 +0800

    test: simplify health controller test setup

--- src/test/java/com/ba/bluearchivemusicapi/controller/HealthControllerTest.java
+++ src/test/java/com/ba/bluearchivemusicapi/controller/HealthControllerTest.java
@@ -4,21 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(
-        useDefaultFilters = false,
-        includeFilters = @ComponentScan.Filter(
-                type = FilterType.REGEX,
-                pattern = ".*HealthController"
-        )
-)
+@WebMvcTest(useDefaultFilters = false)
+@Import(HealthController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class HealthControllerTest {
 
